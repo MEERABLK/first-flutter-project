69c0ea24b1ca1ed66f8a85f55e57b93ba439e809
Meerab Khan <2388387@edu.vaniercollege.qc.ca>
2025-11-25 14:28:04 -0500

    adding google maps api and awesome notifications

---
+++ googleapi/android/app/src/main/kotlin/com/example/googleapi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.googleapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- sampledemo/sampledemo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sampledemo/sampledemo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import awesome_notifications
 import device_info_plus
 import file_selector_macos
 import flutter_local_notifications
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
